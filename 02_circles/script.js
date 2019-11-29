07491d7d135be5c4d351068f35d6e053cef4f8c0
28470 <54799107+28470@users.noreply.github.com>
2019-11-29 11:49:03 +0100

    Challange

--- 02_circles/script.js
+++ 02_circles/script.js
@@ -11,7 +11,7 @@ canvas.width = width;
 canvas.height = height;
 
 // hier kun je op de canvas tekenen
-let colors = ["red", "green", "blue", "yellow", "purple", "pink", "orange"]
+let colors = ["red", "green", "blue", "yellow", "purple", "pink", "orange"];
 
 function getRandomNumber(max){
   return Math.floor(Math.random()*max);
@@ -19,15 +19,13 @@ function getRandomNumber(max){
 
 function drawCircle(){
   context.beginPath();
-  context.fillStyle = colors[getRandomNumber(colors.lenght)];
+  context.fillStyle = colors[getRandomNumber(colors.length)];
   context.arc(getRandomNumber(width),getRandomNumber(height),getRandomNumber(100),0,Math.PI*2)
   context.stroke();
   context.fill();
   context.closePath();
 }
-for(let i = 0; i<100;i++){
-  animate();
-}
+
 function animate(){
   drawCircle();
 }
